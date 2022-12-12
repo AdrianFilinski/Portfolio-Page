4db8e216aa744be7ff96257d1543f55284b4f23b
AdrianFilinski <67971511+AdrianFilinski@users.noreply.github.com>
2022-12-12 19:53:27 +0100

    query empik experience selector

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 const tmobileExperience = document.querySelector('.experience-company__tmobile');
+const empikExperience = document.querySelector('.experience-company__empik');
 const tmobileDescription = document.querySelector('#tmobile_decription');
 const empikDescription = document.querySelector('#empik_decription');
 const date = document.querySelector('.date');
 
 
-const changeContent = () => {
-
-}
