441dc34aba0600447f1f78bd680dcca24c136a7a
AdrianFilinski <67971511+AdrianFilinski@users.noreply.github.com>
2022-12-12 19:50:49 +0100

    get 4 important query

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const tmobileExperience = document.querySelector('.experience-company__tmobile');
+const tmobileDescription = document.querySelector('#tmobile_decription');
+const empikDescription = document.querySelector('#empik_decription');
+const date = document.querySelector('.date');
+
+
+const changeContent = () => {
+
+}
