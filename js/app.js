42d4775532c285aa6982c39922c0cd5cbae6ff1e
AdrianFilinski <67971511+AdrianFilinski@users.noreply.github.com>
2022-12-16 19:53:39 +0100

    create 2 function where change copy on click

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
 const tmobileExperience = document.querySelector('.experience-company__tmobile');
 const empikExperience = document.querySelector('.experience-company__empik');
-const tmobileDescription = document.querySelector('#tmobile_decription');
+const description = document.querySelector('.description');
 const empikDescription = document.querySelector('#empik_decription');
 const date = document.querySelector('.date');
 
+// funckja, która dodaje klasę do experience i usuwa klasę active z drugiego
+// zmienia content w date i description.
+
+const changeCopy = () => {
+    date.textContent = "2021.07 - 2022.02";
+    description.textContent = "ipsum dolor sit amet consectetur adipisicing elitTenetur odio praesentium libero voluptate voluptatibus fugiat nisi accusantium, repellat ducimus nobis nemocorrupti molestiae quasi nesciunt, et voluptatem labore provident maxime.";
+    empikExperience.classList.add('active');
+    tmobileExperience.classList.remove('active');
+};
+
+const setTmobileExperience = () => {
+    date.textContent = "2022.02 - Present";
+    description.textContent = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati laudantium aspernatur facere tempora praesentium tenetur nam, quos nihil voluptas molestiae voluptatem eum ex repellat, esse corporis soluta rem modi ut."
+    empikExperience.classList.remove('active');
+    tmobileExperience.classList.add('active');
+}
+
+tmobileExperience.addEventListener('click', setTmobileExperience)
+empikExperience.addEventListener('click', changeCopy);
 
