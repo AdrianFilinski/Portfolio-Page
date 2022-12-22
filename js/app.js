22294a620e0c56b4546242fa1b265b1f8fc6496f
AdrianFilinski <67971511+AdrianFilinski@users.noreply.github.com>
2022-12-22 21:32:25 +0100

    create experience copy

--- js/app.js
+++ js/app.js
@@ -5,20 +5,23 @@ const empikDescription = document.querySelector('#empik_decription');
 const date = document.querySelector('.date');
 
 const header = document.querySelector('#header');
+const descriptionTools = document.querySelector('.description-tools');
 
-const themeSwitcher = document.querySelector('.theme-switcher');
+
 
 
 const setEmpikExperience = () => {
     date.textContent = "2021.07 - 2022.02";
     description.textContent = "ipsum dolor sit amet consectetur adipisicing elitTenetur odio praesentium libero voluptate voluptatibus fugiat nisi accusantium, repellat ducimus nobis nemocorrupti molestiae quasi nesciunt, et voluptatem labore provident maxime.";
+    descriptionTools.textContent = 'JavaScirpt, HTML, CSS, Synerise, Splio, SMSApi, Firebase, Selligent, SQL, Power BI.'
     empikExperience.classList.add('active');
     tmobileExperience.classList.remove('active');
 };
 
 const setTmobileExperience = () => {
     date.textContent = "2022.02 - Present";
-    description.textContent = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati laudantium aspernatur facere tempora praesentium tenetur nam, quos nihil voluptas molestiae voluptatem eum ex repellat, esse corporis soluta rem modi ut."
+    description.textContent = "Highly skilled in marketing automation, I have a holistic approach to build performant and great frontend campaigns user experiences and business values using"
+    descriptionTools.textContent = 'JavaScirpt, HTML, CSS, Synerise, Exponea, Google Analytics.'
     empikExperience.classList.remove('active');
     tmobileExperience.classList.add('active');
 }
