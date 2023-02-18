a3721e82734b9815df64f3d17a203a887abceb8a
AdrianFilinski <67971511+AdrianFilinski@users.noreply.github.com>
2023-02-18 15:33:03 +0100

    big change in projects

--- js/app.js
+++ js/app.js
@@ -6,27 +6,9 @@ const date = document.querySelector('.date');
 const header = document.querySelector('#header');
 const descriptionTools = document.querySelector('.description--tools');
 
-const setEmpikExperience = () => {
-    date.textContent = "2021.07 - 2022.02";
-    description.textContent = "Creating e-mail, SMS and push campaigns. Conducting A/B/X tests. Coordinating dedicated marketing campaigns. Created complex extensive triggered paths for Marketing Automation using";
-    descriptionTools.textContent = 'HTML, CSS, Synerise, Splio, SMSApi, Firebase, Selligent, SQL, Power BI.'
-    empikExperience.classList.add('select');
-    tmobileExperience.classList.remove('select');
-};
-
-const setTmobileExperience = () => {
-    date.textContent = "2022.02 - Present";
-    description.textContent = "Highly skilled in marketing automation and Developing strategies for using marketing automation and digital channels. Maintenance and optimization of current solutions. I have a holistic approach to build performant and great frontend campaigns user experiences and business values using"
-    descriptionTools.textContent = 'JavaScirpt, HTML, CSS, Synerise, Exponea, Google Analytics.'
-    empikExperience.classList.remove('select');
-    tmobileExperience.classList.add('select');
-}
-
-tmobileExperience.addEventListener('click', setTmobileExperience)
-empikExperience.addEventListener('click', setEmpikExperience);
 
 let i = 0;
-const text = "Adrian Filiński";
+const text = "Jestem Developerem Marketing Automation.";
 let speed = 150;
 
 const typeWriter = () => {
